f6d2173c0ee7107834e2d625804fefed9e2f1328
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-10-10 15:38:11 +0300

    advanced and generics

---
+++ src/advanced/advanced.ts
@@ -0,0 +1,152 @@
+// intersection 
+
+type Admin = {
+    name: string;
+    priviligies: string[]
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+const el: ElevatedEmployee = {
+    name: 'Kir',
+    priviligies: ['deleting'],
+    startDate: new Date()
+};
+
+type strOrNum = string | number;
+type numOrBool = number | boolean;
+type Combine = strOrNum & numOrBool;
+
+// type guards
+
+function add(a: strOrNum, b: strOrNum) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b
+};
+
+add(1,1);
+
+type Unknown = Employee | Admin;
+
+class Car {
+    drive() {
+        console.log('driving car')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('driving truck')
+    }
+    loadCargo(amount: number) {
+        console.log('loading cargo' + amount)
+    };
+};
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(100)
+    };
+};
+
+useVehicle(v1);
+useVehicle(v2);
+
+// discriminated unions
+interface Bird {
+    flyingSpeed: number;
+    kind: 'bird'
+}
+
+interface Horse {
+    runningSpeed: number;
+    kind: 'horse'
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch(animal.kind) {
+        case 'bird':
+            speed = animal.flyingSpeed
+            break;
+        case 'horse':
+            speed = animal.runningSpeed
+            break;
+        default:
+            speed = 'no speed';
+    };
+    return speed;
+};
+
+moveAnimal({ kind: 'bird', flyingSpeed: 10 });
+
+// type casting
+
+const p = document.getElementById('message') as HTMLParagraphElement;
+
+console.dir(p)
+
+// index properties
+
+interface ErrorContainer {
+    [prop: string]: string;
+};
+
+const errorne: ErrorContainer = {
+    email: 'invalid email',
+    phone: 'incorreect phone'
+};
+
+// function overloads
+
+function xFunc(c: string, d: string): string;
+function xFunc(c: string, d: number): string;
+function xFunc(c: number, d: string): string;
+function xFunc(c: number, d: number): number;
+function xFunc(c: strOrNum, d: strOrNum) {
+    if (typeof c === 'string' || typeof d === 'string') {
+        return c.toString() + d.toString()
+    };
+    return c + d
+};
+
+const res = xFunc('hi',' friend');
+const res2 = xFunc(2,10);
+console.log(res2,res2.toExponential(1));
+console.log(res.toUpperCase());
+
+// optional chaining
+
+const fetchedData = {
+    id: '11',
+    name: 'Kirill',
+    job: { title: 'frontend', salary: '3000$' }
+};
+
+console.log(fetchedData?.job?.title);
+
+// nullish coalescing
+
+const userInp = '';
+
+const storedData = userInp || 'DEFAULT';
+
+const nullish = userInp ?? 'DEFAULTT';
+
+console.log(storedData,'=d')
+console.log(nullish,'=> dw')
---
+++ src/generics/generics.ts
@@ -0,0 +1,131 @@
+
+// array
+
+const names: Array<string | number> = ['Kirill','Anna'];
+
+// promise
+
+const promise: Promise<string> = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('done')
+    },1000)
+});
+
+promise.then((res) => {
+    res.toLocaleLowerCase()
+});
+
+// generic function
+
+function merge<T,U>(objA: T,objB: U) {
+    return Object.assign(objA,objB);
+};
+
+// ts infer types
+
+const merged = merge({ name: 'Kirill' },{ age: 19 });
+merged.age = 20
+console.log(merged,'=> merged')
+
+// constraints 
+
+function merge2<T extends object,U extends object>(objA: T,objB: U) {
+    return Object.assign(objA,objB);
+};
+
+const merged2s = merge2({ color: 'red' },{ size: 'medium' });
+
+// one more generic function
+
+interface Lengthy {
+    length: number
+}
+function counterDescriber<T extends Lengthy>(el: T): [T,string] {
+    let disc = 'some text';
+    if(el.length > 0) {
+        disc = 'got els ' + el.length
+    } else if (el.length === 1) {
+        disc = 'got 1 el'
+    } 
+    return [el, disc]
+}
+
+console.log(counterDescriber('Hi ppl'));
+console.log(counterDescriber(['2','3']));
+
+// keyof constraint
+
+function extract<T extends object,U extends keyof T>(obj: T, key: U) {
+    return obj[key]
+};
+
+console.log(extract({ name: 'Kia' },'name'))
+//console.log(extract({ name: 'Kia' },'price'))
+
+// generic classes
+
+class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item)
+    };
+    
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return
+        }
+        this.data.splice(this.data.indexOf(item),1) // -1
+    };
+
+    getItems() {
+        return [...this.data]
+    };
+};
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem('str1');
+textStorage.addItem('str2');
+textStorage.removeItem('str1');
+console.log(textStorage.getItems());
+
+// concrete type
+const numbersStorage = new DataStorage<number>();
+
+// concrete type
+const objectStorage = new DataStorage<object>();
+
+objectStorage.addItem({ name: 'Kirill' });
+objectStorage.addItem({ name: 'Anna' });
+objectStorage.removeItem({ name: 'Kirill'});
+console.log(objectStorage.getItems());
+const kirObject = objectStorage.getItems()[0];
+objectStorage.removeItem(kirObject);
+console.log(objectStorage.getItems());
+
+// generic utility type (built in types)
+
+interface Goal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+// partial
+function createGoal(title: string, description: string, date: Date): Partial<Goal> {
+    return {
+        title,
+        completeUntil: date,
+        description
+    };
+};
+
+// readonly
+
+const cities: Readonly<string[]> = ['Tokyo','New York'];
+//cities.push('Seoul')
+
+// generic types vs utility types
+
+
